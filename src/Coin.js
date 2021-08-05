3e18570eee3729ab94a8b076decc853d7da6fc7c
Amdrew Walsh <andrew.jordan.walsh@gmail.com>
2021-08-04 21:22:02 -0500

    started adding coin cards

--- src/Coin.js
+++ src/Coin.js
@@ -2,11 +2,15 @@ import { CardContent } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 
 export default function Coin(props) {
-    
     return (
         <Card>
             <CardContent>
-                <h5>Coin Name: {props.coinInfo.name} | Coin Price: {props.coinInfo.price}</h5>
+                <h5>{props.coinInfo.name} 
+                <br />
+                  ${props.coinInfo.price}
+                 <br />
+                24hr % Change: {props.coinInfo.percentChange24hr}%
+                 </h5>
             </CardContent>
         </Card>
     )
--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,34 @@ import { useState, useEffect } from 'react';
 import Coin from './Coin';
 import $ from 'jquery';
 import API_BASE_URL from './env';
+import './Home.css'
 
 export default function Home() {
-    
-    const [ coins, setCoins ] = useState([]);
+
+    const [coins, setCoins] = useState([]);
+    const [biggestMovers, setBiggestMovers] = useState([]);
 
     useEffect(() => {
         $.ajax({
             type: 'GET',
             url: API_BASE_URL + '/coins',
         })
-        .then((res) => {
-            setCoins(res.coins);
-        })
+            .then((res) => {
+                console.log(res);
+                setCoins(res.coins);
+                setBiggestMovers(res.biggestMovers);
+            })
     }, []);
 
     return (
-        <div className='d-flex flex-column w-50 m-auto'>
-            {
-                coins.map((coin, key) => {
-                    return <Coin key={key} coinInfo={coin} />
-                })
-            }
+        <div className='cards d-flex w-100 h-100 align-items-center'>
+            <div className='d-flex m-auto'>
+                {
+                    coins.map((coin, key) => {
+                        return <Coin key={key} coinInfo={coin} />
+                    })
+                }
+            </div>
         </div>
     )
 }

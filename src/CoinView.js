8c43b0eb9fc7b4287bf80f4207b65a421cb04270
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-08-21 22:24:57 -0500

    nearly finished with inital version of view for single coin

--- src/CoinView.js
+++ src/CoinView.js
@@ -1,6 +1,7 @@
 import CryptoChart from "./CryptoChart";
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './CoinView.css'
 import $ from 'jquery';
 import API_BASE_URL from './env';
 
@@ -8,6 +9,7 @@ export default function CoinView(props) {
     
     const { coinName } = useParams();
     const [ coinInfo, setCoinInfo ] = useState([]);
+    const [ chartData, setChartData ] = useState([]);
 
     useEffect(() => {
         $.ajax({
@@ -15,13 +17,57 @@ export default function CoinView(props) {
           url: API_BASE_URL + "/coins?coins=" + coinName
         }).then((res) => {
           setCoinInfo(res.coins[0]);
+          let data = []
+          for (let pricePoint of res.coins[0].history) {
+            data.push({
+                'time': pricePoint.date,
+                'value': pricePoint.price   
+            })
+          }
+          setChartData(data);
+          console.log(res);
         })
-      }, []);
+      }, [coinName]);
     
     return (
         <>
-        <CryptoChart coinName={coinName} />
-        <div>{coinInfo.price}</div>
+        <div className='m-auto'>
+          <h1 id='top'>{coinInfo.name}  <span className='text-secondary'>{coinInfo.symbol}</span></h1>
+          <CryptoChart data={chartData} />
+        </div>
+        <div className='d-flex justify-content-center'>
+          <h1>${Number(coinInfo.price).toFixed(2)}</h1>
+          <table id='coin-table'>
+            <tr>
+              <td>
+                <h4>Market Cap</h4>
+              </td>
+              <td>
+                <h5>${numberWithCommas(Number(coinInfo.marketCap).toFixed(2))}</h5>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4>Volume</h4>
+              </td>
+              <td>
+                <h5>{numberWithCommas(Number(coinInfo.volume24hr).toFixed(2))} {coinInfo.symbol}</h5>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4 className='text-center'>24hr Change</h4>
+              </td>
+              <td>
+                <h5 className='text-center'>{Number(coinInfo.percentChange24hr).toFixed(2)}%</h5>
+              </td>
+            </tr>
+          </table>
+        </div>
         </>
     )
+}
+
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
--- src/CryptoChart.js
+++ src/CryptoChart.js
@@ -1,29 +1,16 @@
 import { createChart } from 'lightweight-charts';
 import { useEffect } from 'react';
 import $ from 'jquery';
-import API_BASE_URL from "./env";
 import './CryptoChart.css';
 
 export default function CryptoChart(props) {
 
     useEffect(() => {
-        $.ajax({
-            type: "GET",
-            url: API_BASE_URL + "/coins?coins=" + props.coinName
-          }).then((res) => {
-            let coin = res.coins[0];
-            let data = [];
-            for (let pricePoint of coin.history) {
-                data.push({
-                    'time': pricePoint.date,
-                    'value': pricePoint.price   
-                })
-            }
-            const chart = createChart('chart-container', { width: 800, height: 600 });
+        $('.tv-lightweight-charts').remove();   
+            const chart = createChart('chart-container', { width: 1200, height: 200 });
             const lineSeries = chart.addLineSeries();
-            lineSeries.setData(data);
-          });
-    }, [props.coinName]);
+            lineSeries.setData(props.data);
+    }, [props.data]);
 
     return <div id='chart-container' className='mx-auto d-flex justify-content-center'></div>
 }
